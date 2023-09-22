dd759e066aaf87598a062d2f3e16c27ebbf4909d
Louie <louievw5@gmail.com>
2023-09-21 21:49:02 -0700

    Time: 5081 ms (7.73%), Space: 16.5 MB (90.3%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        base, left, right = 0, 1, len(nums) - 1
+        solutions = []
+        print(nums)
+        while base < len(nums):
+            
+            while left < right:
+                if nums[base] + nums[left] + nums[right] == 0:
+                    val = [nums[base], nums[left], nums[right]]
+                    val.sort()
+                    if val not in solutions:
+                        solutions.append(val)
+                    right -= 1
+                    left += 1
+
+                elif nums[base] + nums[left] + nums[right] < 0:
+                    left += 1
+
+                else: 
+                    right -= 1
+
+            base += 1
+            left = base + 1
+            right = len(nums) - 1
+
+        print(solutions)
+        return solutions
+
