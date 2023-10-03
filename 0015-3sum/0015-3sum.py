71bb8a8bf8a46a5482fb35f92a6ceb5483048ba6
Louie <louievw5@gmail.com>
2023-10-03 12:51:54 -0700

    Time: 5333 ms (5.9%), Space: 16.5 MB (89.38%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -5,29 +5,23 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         nums.sort()
-        base, left, right = 0, 1, len(nums) - 1
         solutions = []
+        base, l, r = 0, 1, 2
 
-        while base < len(nums):
-            
-            while left < right:
-                if nums[base] + nums[left] + nums[right] == 0:
-                    val = [nums[base], nums[left], nums[right]]
-                    val.sort()
-                    if val not in solutions:
-                        solutions.append(val)
-                    right -= 1
-                    left += 1
+        while base < len(nums) - 2:
+            l, r = base + 1, len(nums) - 1
 
-                elif nums[base] + nums[left] + nums[right] < 0:
-                    left += 1
-
-                else: 
-                    right -= 1
+            while l < r:
+                if nums[base] + nums[l] + nums[r] == 0:
+                    if [nums[base], nums[l], nums[r]] not in solutions:
+                        solutions.append([nums[base], nums[l], nums[r]])
+                    l += 1
+                    
+                elif nums[base] + nums[l] + nums[r] > 0:
+                    r -= 1
+                else:
+                    l += 1
 
             base += 1
-            left = base + 1
-            right = len(nums) - 1
 
         return solutions
-
