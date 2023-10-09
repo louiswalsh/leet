ffba16706470872c45e6919668a9421984d5a5a0
Louie <louievw5@gmail.com>
2023-10-09 00:16:15 -0700

    Time: 105 ms (10.76%), Space: 13.4 MB (57.64%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        requiredLetters = {}
+
+        for m in magazine:
+            if m not in requiredLetters.keys():
+                requiredLetters[m] = 1
+            else: 
+                requiredLetters[m] += 1
+
+        for r in ransomNote:
+            if r not in requiredLetters.keys() or requiredLetters[r] == 0:
+                return False
+            
+            requiredLetters[r] -= 1
+
+        print(requiredLetters)
+        return True
